cba33035bfcfa42890184d27d0cf46fbe49bf5f0
alfiannurrizky <alfiannrzky0@gmail.com>
2025-05-18 09:19:09 +0700

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@reoilcollect.com',
+            'password' => Hash::make('password123'),
+        ]);
+    }
+}
